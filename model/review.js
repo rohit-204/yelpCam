ae32222e4aef44ba1348d2c68f8c15b5c225886c
rohit-204 <108423658+rohit-204@users.noreply.github.com>
2022-08-19 12:31:32 +0530

    Add files via upload

---
+++ model/review.js
@@ -0,0 +1,7 @@
+const mongoose= require('mongoose');
+const reviewSchema= new mongoose.Schema({
+    body:String,
+    rating : Number
+})
+const Review= mongoose.model('Review',reviewSchema);
+module.exports=Review;
